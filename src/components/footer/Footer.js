e2e98937cb8e0d8babef043ef1b96e463dc7b3e5
Prashant <prashantsaini4449@gmail.com>
2024-09-17 19:54:34 +0530

    third commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,23 +6,18 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-section footer-left">
-          <a href="#" className="footer-link" style={{display:"flex",alignItems:"center",gap:"5px"}}><FaHome size={24} />Home</a>
-          <a href="#" className="footer-link">Leaderboard</a>
-          <a href="#" className="footer-link">Rewards</a>
-          <a href="#" className="footer-link">Premium Membership</a>
-          <a href="#" className="footer-link">My Leads</a>
-        </div>
-        <div className="footer-section footer-center">
-          <button className="footer-btn">
-            ➕ Add Task
-          </button>
-        </div>
-        <div className="footer-section footer-right">
-          <a href="#" className="footer-link">Settings</a>
-          <a href="#" className="footer-link">Profile</a>
-          <a href="#" className="footer-link">Help</a>
-        </div>
+        <a href="#" className="footer-link" style={{ display: "flex", alignItems: "center", gap: "5px" }}><FaHome size={24} />Home</a>
+        <a href="#" className="footer-link">Leaderboard</a>
+        <a href="#" className="footer-link">Rewards</a>
+        <a href="#" className="footer-link">Premium Membership</a>
+        <a href="#" className="footer-link">My Leads</a>
+
+        <button className="footer-btn">
+          ➕ Add Task
+        </button>
+        <a href="#" className="footer-link">Settings</a>
+        <a href="#" className="footer-link">Profile</a>
+        <a href="#" className="footer-link">Help</a>
       </div>
     </footer>
   );
